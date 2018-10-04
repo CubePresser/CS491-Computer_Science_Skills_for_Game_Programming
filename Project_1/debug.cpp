44302d5b335e7eba315b832d6f541a0439165fd7
jonesjonathan <jonesjon@oregonstate.edu>
2018-10-03 18:53:58 -0700

    Finished project 1 implementation, will test later

--- Project_1/debug.cpp
+++ Project_1/debug.cpp
@@ -10,33 +10,46 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
 
+//Remove these includes
+#include <iostream>
+#include "glm/gtx/string_cast.hpp"
+
+
+
 glm::vec3
 WhatPartOfALivesInBDir( glm::vec3 a, glm::vec3 b )
 {
+	return glm::normalize(b) * glm::dot( a, glm::normalize(b) );
 }
 
 
 glm::vec3
 WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
 {
+	return a  -  WhatPartOfALivesInBDir( a, b );
 }
 
 
 glm::vec3
 UnitSurfaceNormal( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
+	return glm::normalize(glm::cross((r-q),(s-q)));
 }
 
 
 float
 Area( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
+	return glm::length(glm::cross((r-q),(s-q))) * 0.5;
 }
 
 
 bool
 IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
+	glm::vec3 n = glm::cross((r-q), (s-q));
+
+	return ((glm::dot(n, glm::cross((r-q), (p-q)))) > 0) && ((glm::dot(n, glm::cross((s-r), (p-r)))) > 0) && ((glm::dot(n, glm::cross((q-s), (p-s)))) > 0);
 }
 
 
@@ -44,6 +57,7 @@ IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 float
 DistanceFromPointToPlane( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
+	return glm::dot((p-q), UnitSurfaceNormal(q, r, s));
 }
 
 
@@ -52,4 +66,20 @@ WhoAmI( std::string &yourName, std::string &yourEmailAddress )
 {
 	yourName = "Jonathan Jones";
 	yourEmailAddress = "jonesjon@oregonstate.edu" ;
+}
+
+int main()
+{
+	glm::vec3 v1 = glm::vec3(0.0, 0.0, 0.0);
+	glm::vec3 v2 = glm::vec3(1.0, 0.0, 0.0);
+	glm::vec3 v3 = glm::vec3(0.0, 0.0, 1.0);
+
+	glm::vec3 p1 = glm::vec3(0.1, 0.0, 0.1);
+	glm::vec3 p2 = glm::vec3(9182349, 423, 0.5);
+
+	std::cout<<"UnitSurfaceNormal: " + glm::to_string(UnitSurfaceNormal(v1, v3, v2))<<std::endl;
+	std::cout<<"Area: " << Area(v1, v2, v3)<<std::endl;
+	std::cout<<"Point in triangle: p1(" << IsPointInTriangle(v1, v2, v3, p1) << ") p2(" << IsPointInTriangle(v1, v2, v3, p2) << ")\n";
+	std::cout<<"Distance from plane: p2(" << DistanceFromPointToPlane(v1, v3, v2, p2) << ")\n";
+    return 0;
 }
