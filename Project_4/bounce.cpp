156b3c405e38dcbcfc138f61475d1c8e0e901381
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2018-11-11 12:07:49 -0800

    Updated project 4 for correctness

--- Project_4/bounce.cpp
+++ Project_4/bounce.cpp
@@ -1,7 +1,7 @@
 void
-Bounce( float dt )
+Bounce(float dt)
 {
-	while( dt > EPSILON )
+	while (dt > EPSILON)
 	{
 		float tmin = dt;	// minimum time to do something
 		int which = NOTHING_HIT; // which reason was it for doing the something
@@ -9,25 +9,25 @@ Bounce( float dt )
 		// these four collision times are computed using your projectile motion equations:
 
 		float tleft = (XLEFT + RADIUS - Xnow) / Vxnow;	// time to hit the left wall
-		if( tleft > 0.  &&  tleft < tmin )
+		if (tleft > 0.  &&  tleft < tmin)
 		{
 			tmin = tleft;
 			which = HIT_LEFT;
 		}
 		float tright = (XRIGHT - RADIUS - Xnow) / Vxnow;	// time to hit the right wall
-		if( tright > 0.  &&  tright < tmin )
+		if (tright > 0.  &&  tright < tmin)
 		{
 			tmin = tright;
 			which = HIT_RIGHT;
 		}
-		float tfloor1 = (YBOTTOM + RADIUS - Ynow) / Vynow;	// time to hit the floor
-		if( tfloor1 > 0.  &&  tfloor1 < tmin )
+		float tfloor1 = (-Vynow + sqrt((Vynow * Vynow) - ((2 * Gravity)*(Ynow - YBOTTOM - RADIUS)))) / Gravity;	// time to hit the floor
+		if (tfloor1 > 0.  &&  tfloor1 < tmin)
 		{
 			tmin = tfloor1;
 			which = HIT_FLOOR1;
 		}
-		float tfloor2 = (YTOP - RADIUS - Ynow) / Vynow;	// time to hit the floor (note there are 2 answers)
-		if( tfloor2 > 0.  &&  tfloor2 < tmin )
+		float tfloor2 = (-Vynow - sqrt((Vynow * Vynow) - ((2 * Gravity)*(Ynow - YBOTTOM - RADIUS)))) / Gravity;	// time to hit the floor (note there are 2 answers)
+		if (tfloor2 > 0.  &&  tfloor2 < tmin)
 		{
 			tmin = tfloor2;
 			which = HIT_FLOOR2;
@@ -44,47 +44,47 @@ Bounce( float dt )
 		// take a time step of time length tmin, using the projectile motion equations:
 		// if a bounce is going to occur, tmin takes the ball right up to the surface:
 
-		Xnow = Xnow + Vxnow*dt;
+		Xnow = Xnow + Vxnow * tmin;
 		Vxnow = Vxnow;
-		Ynow = Ynow + Vynow*dt + 0.5*Gravity*dt*dt;
-		Vynow = Vynow + Gravity*dt;
+		Ynow = Ynow + Vynow * tmin + 0.5*Gravity*tmin*tmin;
+		Vynow = Vynow + Gravity * tmin;
 
 
 		// a "bounce trick":
 
-		if( Ynow < YBOTTOM+RADIUS )
-			Ynow = YBOTTOM+RADIUS;		// floating point roundoff sometimes messes up
+		if (Ynow < YBOTTOM + RADIUS)
+			Ynow = YBOTTOM + RADIUS;		// floating point roundoff sometimes messes up
 
-		if( Xnow < XLEFT+RADIUS )
-			Xnow = XLEFT+RADIUS;
+		if (Xnow < XLEFT + RADIUS)
+			Xnow = XLEFT + RADIUS;
 
-		if( Xnow > XRIGHT-RADIUS )
-			Xnow = XRIGHT-RADIUS;
+		if (Xnow > XRIGHT - RADIUS)
+			Xnow = XRIGHT - RADIUS;
 
 
 		// depending on what caused the bounce, change the proper velocity component:
 		// if nothing was hit in this time step, just return:
 
-		switch( which )
+		switch (which)
 		{
-			case NOTHING_HIT:
-				return;
+		case NOTHING_HIT:
+			return;
 
-			case HIT_LEFT:
-				Vxnow = -CoefRest * Vxnow;
-				break;
+		case HIT_LEFT:
+			Vxnow = -CoefRest * Vxnow;
+			break;
 
-			case HIT_RIGHT:
-				Vxnow = -CoefRest * Vxnow;
-				break;
+		case HIT_RIGHT:
+			Vxnow = -CoefRest * Vxnow;
+			break;
 
-			case HIT_FLOOR1:
-				Vynow = -CoefRest * Vynow;
-				break;
+		case HIT_FLOOR1:
+			Vynow = -CoefRest * Vynow;
+			break;
 
-			case HIT_FLOOR2:
-				Vynow = -CoefRest * Vynow;
-				break;
+		case HIT_FLOOR2:
+			Vynow = -CoefRest * Vynow;
+			break;
 		}
 
 		dt -= tmin;	// after the bounce, we might still have some time step left
